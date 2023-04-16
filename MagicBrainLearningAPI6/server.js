9defb984b7c459176a7c6cd2437c71029e889f71
Mindaugas Ulonas <mindegi15@gmail.com>
2023-04-16 11:51:46 +0300

    improving Register API

--- MagicBrainLearningAPI6/server.js
+++ MagicBrainLearningAPI6/server.js
@@ -68,12 +68,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    postgres('users').insert({
-        name: name,
-        email: email,
-        joined: new Date()
-    }).then(console.log);
-    res.json(database.users[database.users.length-1]);
+    const hash = bcrypt.hashSync(password);
+    postgres.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0].email,
+                    joined: new Date()
+                })
+                    .then(user => {
+                        res.json(user[0])
+                    });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(error => res.status(400).json('Cannot register'));
 });
 
 app.get('/profile/:id', (req, res) => {
